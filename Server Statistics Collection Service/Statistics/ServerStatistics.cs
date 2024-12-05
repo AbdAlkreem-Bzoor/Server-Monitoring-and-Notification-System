0751ff7f9136a520f9eb493359b682169146c547
Abdalkreem Bzoor <abdalkreembzoor@gmail.com>
2024-12-05 22:37:22 +0200

    Add Task 1: Server Statistics Collection Service

---
+++ Server Statistics Collection Service/Statistics/ServerStatistics.cs
@@ -0,0 +1,22 @@
+﻿using Server_Statistics_Collection_Service.Interfaces;
+using System.Text.Json.Serialization;
+
+namespace Server_Statistics_Collection_Service.Statistics
+{
+    public class ServerStatistics : IServerStatistics
+    {
+        public double MemoryUsage { get; set; } // in MB
+        public double AvailableMemory { get; set; } // in MB
+        public double CpuUsage { get; set; } // percentage
+        public DateTime Timestamp { get; set; }
+        [JsonIgnore]
+        public double CpuUsageInMB => (CpuUsage / 100) * AvailableMemory; // Estimated
+        public override string ToString()
+        {
+            return $"CPU Usage: {CpuUsage}% - {CpuUsageInMB} MB\n" +
+                   $"Available Memory: {AvailableMemory} MB\n" +
+                   $"Memory Usage: {MemoryUsage} MB";
+        }
+    }
+
+}
