a142af66749948a7dc9e6b5b10286862ee6bb93d
Abdalkreem Bzoor <abdalkreembzoor@gmail.com>
2025-12-26 21:58:19 +0200

    Add contract for the publishers

--- MessageBroker.RabbitMQ/Abstractions/IMessagePublisher.cs
+++ MessageBroker.RabbitMQ/Abstractions/IMessagePublisher.cs
@@ -2,6 +2,7 @@
 
 public interface IMessagePublisher
 {
+    Task DeclareExchangeAsync(CancellationToken cancellationToken = default);
     Task PublishAsync<TMeesage>(TMeesage message, CancellationToken cancellationToken = default);
 }
 
